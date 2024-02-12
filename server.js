06601b85f380ab9cbcb109894eb63b401ee20cda
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-12 23:08:51 +0100

    completed math.js

--- server.js
+++ server.js
@@ -6,19 +6,27 @@ console.log('Hello bungong')
 //console.log(global);
 // 4) Has common core modules that we will explore
 // 5) CommonJS modules instead of ES6 modules
+// 6) Missing some JS APIS like fetch 
 
 const os = require('os');
 const path = require('path')
 
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+const {add, subtract, multiply, divide} = require('./math')
 
-console.log(__dirname)
-console.log(__filename)
+console.log(add(2,5))
+console.log(subtract(2,5))
+console.log(multiply(2,5))
+console.log(divide(2,5))
 
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename))
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
 
-console.log(path.parse(__filename))
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+// console.log(path.parse(__filename))

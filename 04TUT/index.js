f12f6128d2c3ce48133dee52e6a0c469d9b84440
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-14 12:32:25 +0100

    completed lesson 5

---
+++ 04TUT/index.js
@@ -0,0 +1,18 @@
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listerner for the log event
+myEmitter.on('log', (msg)=>logEvents(msg));
+
+
+setTimeout(() => {
+    // Emit event
+    myEmitter.emit('log', 'Log event emitted!');
+}, 2000);

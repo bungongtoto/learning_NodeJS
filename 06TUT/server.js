426c3437bbfd63a45838ccc06a8ff65f7873412a
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-19 10:34:24 +0100

    completed lesson 7

--- 06TUT/server.js
+++ 06TUT/server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
+
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));

66487bc2a188978042fb9ef5948aa6068ba43e11
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-02-19 22:42:41 +0100

    completed lesson 8

---
+++ 08TUT/routes/root.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('^/$|/index(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname,'..', 'views', 'index.html'));
+});
+
+router.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname,'..', 'views', 'new-page.html'));
+});
+
+router.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); //302 by default
+});
+
+
+module.exports = router;
